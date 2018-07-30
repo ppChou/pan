1e18db55a160a427299aa01470446ce2ce475fca
liuhuixin <huixin@huixindeMacBook-Pro.local>
2018-07-30 18:18:55 +0800

    jpa

--- src/main/java/com/lhx/controller/SampleController.java
+++ src/main/java/com/lhx/controller/SampleController.java
@@ -1,14 +1,20 @@
 package com.lhx.controller;
 
+import com.lhx.model.User;
+import com.lhx.repository.UserRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.annotation.Resource;
+import java.util.List;
+
 @Controller
 @EnableAutoConfiguration
 public class SampleController {
+
     @RequestMapping("/")
     @ResponseBody
     String home() {
